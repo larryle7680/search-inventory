462cabe715dc6617a57d8cafb78262a0cde4c305
Larry <larryle7680@gmail.com>
2026-04-23 12:59:51 -0500

    Included a addInventory method to add items to the list.
    Also Finish priceSearc Method. Added the range and print the between values

--- src/main/java/com/pluralsight/SearchInventoryApp.java
+++ src/main/java/com/pluralsight/SearchInventoryApp.java
@@ -1,5 +1,9 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.*;
 
@@ -19,21 +23,25 @@ static Scanner theScanner = new Scanner (System.in);
         for(int i = 0; i < inventory.size(); i++) {
             Product p = inventory.get(i);
             System.out.printf("ID: %d | Name: %s | Price: %.2f\n",p.getProductID(),p.getProductName(),p.getPrice());
-
-
         }
+
+        //Call the menu
+        //Prompt the user
+        inventoryMenu();
     }
 
 
 
     public static void inventoryMenu(){
         //Display the Inventory menu
+        System.out.println();
         System.out.println("=== Inventory Display ===");
         System.out.println();
-        System.out.println("Press 1 to Display ID");
-        System.out.println("Press 2 to Display Name");
-        System.out.println("Press 3 to Display Price");
-        System.out.println("Press 4 to exit");
+        System.out.println("1: List all items");
+        System.out.println("2: Search by ID");
+        System.out.println("3: Search by Name");
+        System.out.println("4. Search by Price Range");
+        System.out.println("5. Add items to Inventory");
         System.out.println();
         System.out.println("Please choose an option: ");
 
@@ -45,16 +53,22 @@ static Scanner theScanner = new Scanner (System.in);
 
             switch(usersChoice){
                 case 1:
-                    idSearch;
+                    //allItems();
                     break;
                 case 2:
-                    nameSearch;
+                    idSearch();
                     break;
                 case 3:
-                    priceSearch;
+                    nameSearch();
                     break;
                 case 4:
-                    isRunning = false;
+                    priceSearch();
+                    break;
+                case 5:
+                    addInventory(isRunning);
+                    break;
+                case 6:
+                    System.exit(0);
                 default:
                     System.out.println("Invalid choice");
             }
@@ -76,6 +90,7 @@ static Scanner theScanner = new Scanner (System.in);
         return inventory;
     }
 
+    //ID SEARCH METHOD
     public static void idSearch(){
         //Setting Variables
         boolean found = false;
@@ -91,15 +106,116 @@ static Scanner theScanner = new Scanner (System.in);
                 for(int i = 0; i < inventory.size(); i++) {
                 Product p = inventory.get(i);
                 if(searchId == p.getProductID()){
+                    System.out.println();
+                    System.out.println("Your ID matches this item:");
                     System.out.println(p.getProductID() + " | " + p.getProductName() + " | " + p.getPrice());
 
                 }
                 }
+                }
 
+    }
+    //NAME SEARCH METHOD
+    public static void nameSearch(){
+        //Setting Variables
+        boolean found = false;
+        ArrayList<Product> inventory = getInventory();
+
+
+        //Using a while loop to prompt user the questions
+        while (!found){
+            theScanner.nextLine();
+            System.out.print("Please input your item Name: ");
+            String searchId = theScanner.nextLine();
+
+            //Use a for loop to iterate through the array and print
+            for(int i = 0; i < inventory.size(); i++) {
+                Product p = inventory.get(i);
+                if(searchId.equalsIgnoreCase(p.getProductName()) ){
+                    System.out.println();
+                    System.out.println("Your name input matches this item:");
+                    System.out.println(p.getProductID() + " | " + p.getProductName() + " | " + p.getPrice());
+
+                }
             }
-
-
         }
+
+    }
+    //PRICE SEARCH METHOD
+    public static void priceSearch(){
+        //Setting Variables
+        boolean found = false;
+        ArrayList<Product> inventory = getInventory();
+
+
+        //Using a while loop to prompt user the questions
+        while (!found){
+            System.out.print("Please input your item price minimum: ");
+            double searchPriceMin = theScanner.nextDouble();
+            System.out.println();
+            System.out.println("Please input your item price maximum: ");
+            double searchPriceMax = theScanner.nextDouble();
+
+
+            //Use a for loop to iterate through the array and print
+            for(int i = 0; i < inventory.size(); i++) {
+                Product p = inventory.get(i);
+                if(p.getPrice() >= searchPriceMin && p.getPrice() <= searchPriceMax ){
+                    System.out.println();
+                    System.out.println("These are the items that are within the range: ");
+                    System.out.println(p.getProductID() + " | " + p.getProductName() + " | " + p.getPrice());
+
+
+
+                }
+            }
+        }
+
+    }
+    //ADD ITEMS METHOD
+    public static void addInventory( boolean isRunning){
+        ArrayList<Product> inventory = getInventory();
+        //Inventory menu prompt user to fill out the questions and store to add new items.
+        System.out.println("=== Adding Inventory ===");
+        System.out.println();
+        System.out.println("What is the item ID?");
+        int addItemId = theScanner.nextInt();
+
+        //Eat line
+        theScanner.nextLine();
+
+        System.out.println("What is the name of the item?");
+        String addItemName = theScanner.nextLine();
+        System.out.println("What is the price of the item?");
+        double addItemPrice = theScanner.nextDouble();
+
+        try {
+            FileWriter fileWriter = new FileWriter("src/main/inventory.csv");
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+            Product addInventoryItems = new Product(addItemId,addItemName,addItemPrice);
+
+            bufWriter.write(addInventoryItems.getProductID() + "|" + addInventoryItems.getProductName() + "|" + addInventoryItems.getPrice() + true);
+
+            bufWriter.close();
+            System.out.println();
+            System.out.println("You added: " + "ID: " + addInventoryItems.getProductID() + " Name: " + addInventoryItems.getProductName() + " Price: " + addInventoryItems.getPrice());
+            System.out.println();
+            //Close the menu
+            isRunning = false;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+    }
+
+
+
 }
+
 
 
